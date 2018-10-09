0b4d5889e169835a245dab752e273c07b59ae15d
George Benjamin <msdcconnect@gmail.com>
2018-10-09 12:28:33 +0100

    Changed script tag from head to the end of the body tag

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
+//Button to add tasks to the taskList
+var add_button = document.getElementById('add-button');
+
+//Field for the user to type in tasks
+var inputField = document.getElementById('input');
+
+//Adds task to the taskList when the add_button is clicked
+add_button.addEventListener('click', add_new);
+
+//Object to store all the tasks added by the user
+var taskData = (localStorage.getItem('tasks'))? JSON.parse(localStorage.getItem('tasks')) : {
+    listOfTasks:[]
+}
+
 //function to add a new todo
 function add_new(){
+        console.log("Button tappped")
 
 	  //create a variable for the ul
 	  var ul = document.getElementById("todo");
