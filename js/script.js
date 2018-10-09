214997d6eab5cbfbe963d52b3792c1b2481ecfec
George Benjamin <msdcconnect@gmail.com>
2018-10-09 13:06:30 +0100

    Stored todo items locally on the user's device

--- js/script.js
+++ js/script.js
@@ -8,19 +8,55 @@ var inputField = document.getElementById('input');
 add_button.addEventListener('click', add_new);
 
 //Object to store all the tasks added by the user
-var taskData = (localStorage.getItem('tasks'))? JSON.parse(localStorage.getItem('tasks')) : {
-    listOfTasks:[]
+var todoItems = (localStorage.getItem('todo'))? JSON.parse(localStorage.getItem('todo')) : {
+    listOfTodo:[]
+}
+
+displayData()
+
+function storeDataLocally(){
+    //JSON.stringify() converts a javascript object to JSON and stores it in local storage
+    localStorage.setItem('todo', JSON.stringify(todoItems));
+}
+
+function displayData(){
+    //Displays the data in local storage on the DOM
+    
+    //If there are no tasks, end function
+    if(!todoItems.listOfTodo.length) return;
+    
+    //Add tasks from the uncompletedTasks array
+    for(let i = 0; i < todoItems.listOfTodo.length; i++){
+        let task = todoItems.listOfTodo[i];
+        console.log(task)
+        addItemToDOM(task);
+    }
+    
 }
 
 //function to add a new todo
 function add_new(){
-        console.log("Button tappped")
+    //console.log("Button tappped")
+   
+    
+    //get the text the user inputted into the text-input
+	  var todo_text = inputField.value;
+    todo_text = todo_text.trim();
+    console.log(todo_text)
+    
+    if(todo_text){
+        //The user input was valid
+        todoItems.listOfTodo.push(todo_text);
+        console.log(todoItems.listOfTodo)
+        addItemToDOM(todo_text);
+        storeDataLocally()
+        inputField.value = ''
+    }
 
 	  //create a variable for the ul
-	  var ul = document.getElementById("todo");
+	  /*var ul = document.getElementById("todo");
 
-	  //get the text the user inputted into the text-input
-	  var todo_text = document.getElementById('input').value;
+	  
 
    	  //created a new list-item
 	  var li = document.createElement("li");
@@ -32,5 +68,41 @@ function add_new(){
 	  ul.appendChild(li);
 
 	  //revert the text in the text-input to ''
-	  document.getElementById('input').value = '';
+	  document.getElementById('input').value = '';*/
+}
+
+function addItemToDOM(task){
+    
+    var list_item = document.createElement('li');
+    //list_item.classList.add('todo-list');
+    list_item.innerHTML = task;
+    
+    /*var buttons = document.createElement('div');
+    buttons.classList.add('buttons');*/
+    
+    /*var delete_btn = document.createElement('button');
+    delete_btn.innerHTML = "<img src='images/delete_button.png'>";
+    delete_btn.addEventListener('click', removeTodoItem);
+    
+    var done_btn = document.createElement('button');
+    done_btn.innerHTML = "<img src='images/done_icon.png'>";
+    done_btn.addEventListener('click', completeTodo);*/
+    
+    var taskList = document.getElementById("todo")
+    
+    /*if(completed  && !showCompleteLabel){
+        var completedLabel = document.createElement('h4');
+        completedLabel.appendChild(document.createTextNode("Completed tasks"));
+        document.getElementById('todo').appendChild(completedLabel);
+        
+        showCompleteLabel = true;
+    }*/
+    
+    /*buttons.appendChild(delete_btn);
+    buttons.appendChild(done_btn);
+    
+    list_item.appendChild(buttons);*/
+    
+    //Adds the new list item to the top of the list
+    taskList.insertBefore(list_item, taskList.childNodes[0]);
 }
